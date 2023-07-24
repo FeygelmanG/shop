04835f92a95155aad3a2366f77d5923ef67de8d1
Gulnara Feygelman <gulya_b@yahoo.com>
2023-07-24 23:15:36 +0300

    add All RestAssured tests & Unit tests + Allure

--- src/test/java/com/example/shop/ShopControllerTests.java
+++ src/test/java/com/example/shop/ShopControllerTests.java
@@ -1,6 +1,6 @@
 package com.example.shop;
 
-import com.example.shop.models.ShopDto;
+import com.example.shop.models.ShopPojo;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -8,16 +8,19 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import java.util.Objects;
 
-import static io.restassured.parsing.Parser.JSON;
+import static com.example.shop.Configuration.buildFactory;
+import static com.example.shop.Configuration.createNewSession;
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.Matchers.equalTo;
 
 
 public class ShopControllerTests {
-    private final Faker faker = new Faker();
+    private static final Faker faker = new Faker();
     RequestSpecification requestSpec;
     ResponseSpecification responseShopDto;
 
@@ -35,58 +38,9 @@ public class ShopControllerTests {
         responseShopDto = specBuilder.build();
     }
 
-    //4. Должен быть запрос для поиска, который ищет конкретный магазин по айди.
-    //Выводит результат, если айди существует, в виде айди, названия и настройки
-    //приватности магазина
     @Test
-    public void shouldGetShopByIdTest() {
-        requestSpec
-                .get("/shops/{shopId}", 5753)
-                .then()
-                .spec(responseShopDto)
-                .body("shopName", equalTo("TestShop"));
-    }
-
-    //3. Должен быть запрос для вывода всех существующих магазинов, который выводит айди, название и настройку
-// приватности магазина(публичный/не публичный)
-    @Test
-    public void shouldGetAllShop() {
-        Response response = requestSpec.get("/shops/all");
-        response.then().statusCode(200);
-    }
-
-    //1. Должна быть возможность создавать магазины:
-//    a. Один запрос = один новый магазин
-//    b. Имя магазина должно быть больше 6 символов
-//    c. Имя магазина может содержать любые символы, но обязано начинаться
-//    с большой буквы
-//    d. Длина имени ограничена 256 символами
-//    e. При нарушении вышеперечисленных требований выводится
-//    соответствующая ошибка (к текстам ошибок требований нет, но ошибка
-//    должна быть обработана, то есть пользователю понятно, что пошло не
-//    так, а не просто 500 или 400, не важно на бэке или на фронте)
-    @Test
-    public void shouldAddRandomShop() {
-        Response response = requestSpec.post("/shops/add");
-        response.then().statusCode(200);
-    }
-
-    //  1. Должна быть возможность создавать магазины:
-//    a. Один запрос = один новый магазин
-    @Test
+    @DisplayName("Добавление тестового магазина")
     public void shouldAddShop() {
-        //        JSONObject shop = new JSONObject()
-//                .put("shopName", "Shop Name")
-//                .put("shopPublic", false);
-//
-//        ShopDto shopRes = requestSpec
-//                .body(shop.toString())
-//                .contentType("application/json")
-//                .post("/shops/add")
-//                .as(new TypeRef<>() {});
-//
-//        assertThat(shopRes.getShopId().equals());
-
         String shopName = faker.funnyName().name();
         JSONObject object = new JSONObject();
         object.put("shopName", shopName);
@@ -94,39 +48,115 @@ public class ShopControllerTests {
 
         requestSpec.body(object.toString());
         requestSpec.header("content-type", "application/json");
-        System.out.println(object);
         Response response = requestSpec.post("/shops/add");
-//        System.out.println(response.then().extract().response().as(String.class));
         response.then()
                 .statusCode(200);
-//                .assertThat()
-//                .body("shopName", equalTo(shopName));
 
     }
-//2. Должна быть возможность удалять магазины:
-//    a. Один запрос = один удаленный магазин
-//    b. При попытке удалить магазин без указания айди на фронтенде
-//    появляется соответствующая ошибка, на бэкенде не должно быть
+
     @Test
-    public void shouldDeletePet() {
+    @DisplayName("Негативная проверка добавления тестового магазина с именем > 256 символов")
+    public void shouldNotAddShopWithExtraLongName() {
+        String shopName = "s".repeat(256) +
+                faker.funnyName().name();
+        JSONObject object = new JSONObject();
+        object.put("shopName", shopName);
+        object.put("shopPublic", faker.random().nextBoolean());
 
-//        JSONObject shop = new JSONObject()
-//                .put("id", 8054L)
-//                .put("shopName", "Ben Down")
-//                .put("shopPublic", "false");
+        requestSpec.body(object.toString());
+        requestSpec.header("content-type", "application/json");
+        Response response = requestSpec.post("/shops/add");
+        response.then()
+                .statusCode(400);
 
-//        ShopDto createdShop = generateShop(shop);
-
-        requestSpec
-                .delete("/shops/delete/{id}", 8054L)
-                .then()
-                .statusCode(204);
     }
-//    private ShopDto generateShop(Object o) {
-//        return request
-//                .body(o.toString())
-//                .contentType("application/json")
-//                .post("/shop")
-//                .as(new TypeRef<>() {});
-//    }
+
+    @Test
+    @DisplayName("Получение всех магазинов")
+    public void shouldGetAllShop() {
+        Response response = requestSpec.get("/shops/all");
+        response.then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Получение магазина по id")
+    public void shouldGetShopByIdTest() {
+        String shopName = faker.funnyName().name();
+        Boolean shopPublic = faker.random().nextBoolean();
+
+        step("Создать тестовый магазин", () -> {
+            JSONObject object = new JSONObject();
+            object.put("shopName", shopName);
+            object.put("shopPublic", shopPublic);
+
+            requestSpec.body(object.toString());
+            requestSpec.header("content-type", "application/json");
+            Response response = requestSpec.post("/shops/add");
+            response.then()
+                    .statusCode(200);
+        });
+        step("Получить магазин по полученному id тестового магазина", () -> {
+            SessionFactory factory = buildFactory();
+            Session session = createNewSession(Objects.requireNonNull(factory));
+            Long shopId = 0L;
+            var shopsList = session.createNativeQuery("SELECT * FROM shops ORDER BY shop_id DESC", ShopPojo.class).list();
+            Assertions.assertTrue(shopsList.size() > 0);
+            int i = shopsList.size() - 1;
+            String shopNameSelect = "";
+            while ((i >= 0) & (!shopNameSelect.equals(shopName))) {
+                ShopPojo testShop = shopsList.get(i);
+                shopNameSelect = testShop.getShopName();
+                shopId = testShop.getShopId();
+                i--;
+            }
+            session.close();
+
+            requestSpec
+                    .get("/shops/{shopId}", shopId)
+                    .then()
+                    .spec(responseShopDto)
+                    .body("shopName", equalTo(shopName), "shopPublic", equalTo(shopPublic));
+        });
+    }
+
+    @Test
+    @DisplayName("Удаление тестового магазина")
+    public void shouldDeleteShop() {
+        String shopName = faker.funnyName().name();
+
+        step("Создать тестовый магазин", () -> {
+            JSONObject object = new JSONObject();
+            object.put("shopName", shopName);
+            object.put("shopPublic", faker.random().nextBoolean());
+
+            requestSpec.body(object.toString());
+            requestSpec.header("content-type", "application/json");
+            Response response = requestSpec.post("/shops/add");
+            response.then()
+                    .statusCode(200);
+        });
+
+        step("Удалить магазин по полученному id тестового магазина", () -> {
+            SessionFactory factory = buildFactory();
+            Session session = createNewSession(Objects.requireNonNull(factory));
+            Long shopId = 0L;
+            var shopsList = session.createNativeQuery("SELECT * FROM shops ORDER BY shop_id DESC", ShopPojo.class).list();
+            Assertions.assertTrue(shopsList.size() > 0);
+            int i = shopsList.size() - 1;
+            String shopNameSelect = "";
+            while ((i >= 0) & (!shopNameSelect.equals(shopName))) {
+                ShopPojo testShop = shopsList.get(i);
+                shopNameSelect = testShop.getShopName();
+                shopId = testShop.getShopId();
+                i--;
+            }
+            session.close();
+
+            requestSpec
+                    .delete("/shops/delete/{id}", shopId)
+                    .then()
+                    .statusCode(204);
+        });
+
+    }
 }
--- src/test/java/com/example/shop/ShopHandlerTests.java
+++ src/test/java/com/example/shop/ShopHandlerTests.java
@@ -1,45 +1,46 @@
 package com.example.shop;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 import static com.example.shop.ShopHandler.checkFirstLetter;
 import static com.example.shop.ShopHandler.checkLength;
 
 
 public class ShopHandlerTests {
-//  1.c. Имя магазина может содержать любые символы, но обязано начинаться с большой буквы
-//  Позитивный
     @ParameterizedTest
     @ValueSource(strings = {"Sporttovari", "InnopolisMarket", "ArskiiMed"})
+    @DisplayName("Положительная роверка, что имя магазина может содержать любые символы, но обязано начинаться с " +
+            "большой буквы")
     public void shouldFirstLetter(String nameShop) {
 
         Assertions.assertTrue(checkFirstLetter(nameShop));
     }
 
-//  1.c. Имя магазина может содержать любые символы, но обязано начинаться с большой буквы
-//  Негативный
+    @DisplayName("Негативная проверка, что имя магазина может содержать любые символы, но обязано начинаться с " +
+            "большой буквы")
     @ParameterizedTest
     @ValueSource(strings = {"sporttovari", "innopolisMarket", "arskiiMed"})
     public void shouldNotFirstLetter(String nameShop) {
 
         Assertions.assertFalse(checkFirstLetter(nameShop));
     }
-//  1.b. Имя магазина должно быть больше 6 символов
-//  Позитивный
+
+    @DisplayName("Позитивная проверка, что имя магазина должно быть больше 6 символов")
     @ParameterizedTest
     @ValueSource(strings = {"Sporttovari", "InnopolisMarket", "ArskiiMed"})
     public void shouldLengthShop(String nameShop) {
 
         Assertions.assertTrue(checkLength(nameShop, 7));
     }
-//  1.b. Имя магазина должно быть больше 6 символов
-//  Негативный
+
+    @DisplayName("Негативная проверка, что имя магазина должно быть больше 6 символов")
     @ParameterizedTest
     @ValueSource(strings = {"Sport", "Inno", "Arsk"})
     public void shouldNotLengthShop(String nameShop) {
 
         Assertions.assertFalse(checkLength(nameShop, 7));
     }
-
 }
