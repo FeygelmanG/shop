e1f4c2b2b720371251d0e0c56c129e61d8bfdedf
Gulnara Feygelman <gulya_b@yahoo.com>
2023-07-27 21:18:24 +0300

    fixed build.gradle

--- src/test/java/com/example/shop/ShopWebTests.java
+++ src/test/java/com/example/shop/ShopWebTests.java
@@ -1,4 +1,39 @@
 package com.example.shop;
 
-public class ShopWebTests {
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeClass;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class ShopWebTests extends BaseTest{
+    @BeforeClass
+    public static void beforeClass() {
+    }    @BeforeEach
+    public void setSelenide() {
+        open("http://localhost:4000");
+    }
+
+    @Test
+    @DisplayName("Удаление магазина")
+    public void shouldSearchProductsFivePosition() {
+
+        step("Ввести \"8974\" в поле Id для 'Delete a shop'", () -> inputDeleteId("8974"));
+
+        step("Проверить, что в списке магазинов не отображается магазин с id \"8974\"", () -> {
+            ElementsCollection itemsSearch = $$x("//tbody[@id=\"response\"]");
+            itemsSearch.shouldBe(CollectionCondition.size(5));
+        });
+    }
+    public void inputDeleteId(String textId) {
+        SelenideElement deleteField = $x("//input[@id=\"id\"]");
+        deleteField.sendKeys(textId);
+        SelenideElement magnifier = $x("//button[@onclick=\"deleteShop();\"]");
+        magnifier.click();
+    }
+
 }
