8fbb11bf78cb80888abc75da4e359ccf661500f4
Gulnara Feygelman <gulya_b@yahoo.com>
2023-08-04 00:28:48 +0300

    fix withoutWebTest

--- src/test/java/com/example/shop/BaseTest.java
+++ src/test/java/com/example/shop/BaseTest.java
@@ -14,21 +14,21 @@ import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 public class BaseTest {
 
-    @BeforeAll
-    public static void setDriver() throws MalformedURLException {
-        String isRemote = System.getenv("IS_REMOTE");
-        if (Objects.equals(isRemote, "true")) {
-            Configuration.timeout = 6000;
-            Configuration.browser = "chrome";
-            Configuration.browserSize = "1920x1080";
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.setCapability("enableVNC:", true);
-            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), chromeOptions);
-            setWebDriver(driver);
-        } else {
-            Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-            Configuration.browser = "Chrome";
-        }
-    }
+//    @BeforeAll
+//    public static void setDriver() throws MalformedURLException {
+//        String isRemote = System.getenv("IS_REMOTE");
+//        if (Objects.equals(isRemote, "true")) {
+//            Configuration.timeout = 6000;
+//            Configuration.browser = "chrome";
+//            Configuration.browserSize = "1920x1080";
+//            ChromeOptions chromeOptions = new ChromeOptions();
+//            chromeOptions.setCapability("enableVNC:", true);
+//            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), chromeOptions);
+//            setWebDriver(driver);
+//        } else {
+//            Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+//            Configuration.browser = "Chrome";
+//        }
+//    }
 }
 
